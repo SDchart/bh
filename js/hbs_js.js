66a8b0bd865f78fffcf3f7d9e673bd55a7e3999e
SDchart <369999245@qq.com>
2019-05-11 17:22:20 +0800

    update

---
+++ js/hbs_js.js
@@ -0,0 +1,8 @@
+var loginBtn=document.querySelector("li.bhs_header_aside_lang_en");
+loginBtn.onclick=function () {
+    alert("sa");
+    var leftPic=document.querySelector("div.bhs_headerWrap");
+    console.log(leftPic);
+    var rightPic=document.querySelector("div.rightPic");
+    var blingbling=document.querySelector("span.blingbling");
+}
