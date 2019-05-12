fb10562e2007c12856fd917ccd1f31bba24ffcdd
SDchart <369999245@qq.com>
2019-05-13 00:08:37 +0800

    update

--- js/hbs_js.js
+++ js/hbs_js.js
@@ -4,13 +4,11 @@ var leftPic=document.querySelector("div.leftPic");
 var rightPic=document.querySelector("div.rightPic");
 var loginPanel=document.querySelector("div.bhs_loginPanel");
 loginBtn.onclick=function () {
-    alert("sa");
     leftPic.setAttribute("style","animation: trans1_1 1.3s forwards");
     rightPic.setAttribute("style","animation: trans2_1 1.3s forwards");
     loginPanel.setAttribute("style","animation: loginPanel 0.7s 1.3s forwards");
 }
 escBtn.onclick=function () {
-    alert("as");
     leftPic.setAttribute("style","animation: trans1 1.3s 0.7s forwards");
     rightPic.setAttribute("style","animation: trans2 1.3s 0.7s forwards");
     loginPanel.setAttribute("style","animation: loginPanel_1 0.7s forwards");
